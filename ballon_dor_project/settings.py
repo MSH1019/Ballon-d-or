c2298088d78446ebaf33b7dd2404676ef04d10c8
MSH1019 <2833753s@student.gla.ac.uk>
2025-08-05 11:38:05 +0200

    fixing up the settings for testing

--- ballon_dor_project/settings.py
+++ ballon_dor_project/settings.py
@@ -35,8 +35,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 #   "127.0.0.1",
 # ]
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
