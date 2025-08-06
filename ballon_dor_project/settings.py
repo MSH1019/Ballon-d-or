554787a95b7fc8625623acd4358ed623074c4ac4
MSH1019 <2833753s@student.gla.ac.uk>
2025-08-06 14:00:52 +0200

    Configure email settings for contact@fansaward.com

--- ballon_dor_project/settings.py
+++ ballon_dor_project/settings.py
@@ -180,9 +180,9 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "mail.fansaward.com"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = "contact@fansaward.com"
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = "FansAward <contact@fansaward.com>"
